8d9ffce8465ef771f071ed5f21bb84fe4350dd86
Joshua Malomo <rexunborn@gmail.com>
2020-08-19 22:56:26 +0100

    Started Calc

---
+++ calc.js
@@ -0,0 +1,13 @@
+const add = ((a, b) => a + b);//Calc functions
+const subtract = ((a, b) => a - b);
+const multiply = ((a, b) => a * b);
+const divide = ((a, b) => a/b);
+
+const operate = ((a, b, operator) => {
+    if (operator == add || operator == subtract || operator == multiply || operator == divide) {
+        return operator(a, b);
+    };
+});
+
+const container = document.querySelector('.container');
+const 
